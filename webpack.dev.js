1180e418558a82d3b174c7319475d1b80cedfd70
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2024-09-14 17:56:53 -0300

    first commit

---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+
+
+module.exports = merge(common, {
+
+    mode: 'development',
+
+    devtool: 'inline-source-map',
+        devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+
+    devServer: {
+        static: './dist',
+    },
+
+});
